ac507cebce05bb60b95f05a6333420c552e5cab3
AYETEEN7 <ayeteen1@gmail.com>
2025-06-10 05:35:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function toggleMenu() {
     document.querySelector('.header nav').classList.toggle('active');
 }
+window.addEventListener('scroll', function() {
+    let logo = document.getElementById('logo');
+    
+    if (window.scrollY > 50) { // You can adjust 50 to control sensitivity
+        logo.classList.add('fade');
+    } else {
+        logo.classList.remove('fade');
+    }
+});
